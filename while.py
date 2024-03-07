044b8cf3e5bef039d8a02f79b229c8233fa68d00
*★,°𝑰𝒔𝒂°★* 。 <162610401+saythename127@users.noreply.github.com>
2024-03-07 14:00:45 -0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,28 @@
+x=1
+while x<=100:
+     print(x)
+     x=x+1
+
+#Imprimir los números del 1 al 500.
+x=1
+while x<=500:
+     print(x)
+     x=x+1
+     
+#Imprimir los números del 50 al 100.
+x=50
+while x<=100:
+     print(x)
+     x=x+1
+
+#Imprimir los números del -50 al 0.
+x=-50
+while x<=0:
+     print(x)
+     x=x+1
+
+#Imprimir los números del 2 al 100 pero de 2 en 2 (2,4,6,8 ....100).
+x=2
+while x<=100:
+     print(x)
+     x=x+2
